d82e469a844a28ca8604c6de3733de0ab07e700a
serieys <serieys.t@gmail.com>
2017-06-22 21:58:51 +0200

    Added first try to evaluate the board

--- js/ia.js
+++ js/ia.js
@@ -4,19 +4,37 @@ var calculateBestMove =function(game) {
     var bestMove = null;
     var bestValue = -9999;
 
-    // for (var i = 0; i < newGameMoves.length; i++) {
-    //     var newGameMove = newGameMoves[i];
-    //     game.move(newGameMove);
-    //
-    //     //take the negative as AI plays as black
-    //     var boardValue = -evaluateBoard(game.board())
-    //     game.undo();
-    //     if (boardValue > bestValue) {
-    //         bestValue = boardValue;
-    //         bestMove = newGameMove
-    //     }
-    // }
+    for (var i = 0; i < newGameMoves.length; i++) {
+        var newGameMove = newGameMoves[i];
+        game.move(newGameMove);
+
+        //take the negative as AI plays as black
+        var boardValue = -evaluateBoard(game);
+        game.undo();
+        if (boardValue > bestValue) {
+            bestValue = boardValue;
+            bestMove = newGameMove
+        }
+    }
 
     game.move(newGameMoves[Math.floor(Math.random() * newGameMoves.length)]);
     board.position(game.fen());
 };
+
+var evaluateBoard = function(game) {
+  for (var x = 1; x <= 8; x++) {
+    for (var y = 1; y <= 8; y++) {
+      game.get((toLetters(x)+y).toLowerCase());
+    }
+  }
+
+}
+
+
+function toLetters(num) {
+    "use strict";
+    var mod = num % 26,
+        pow = num / 26 | 0,
+        out = mod ? String.fromCharCode(64 + mod) : (--pow, 'Z');
+    return pow ? toLetters(pow) + out : out;
+}
